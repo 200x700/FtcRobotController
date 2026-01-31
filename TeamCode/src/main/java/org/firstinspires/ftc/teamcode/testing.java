3f10c36b885b2dcc4a5b6ed87aa4c6bec74ac485
200x700 <110259236+200x700@users.noreply.github.com>
2026-01-31 12:41:35 +0200

    Create testing.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous
+public class testing extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello","Ofir"); // Hello
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+}
